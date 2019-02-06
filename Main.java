38e7c5010a7c1795bbb9bbdca20ed5eec8236cfa
Leah <leah29726@gmail.com>
2019-02-05 22:19:13 -0500

    Alice and Bob are here

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+     System.out.println("Enter a name");
+     Scanner oww = new Scanner(System.in);
+     String name = oww.nextLine();
+     
+     if(name.equals("Alice") || name.equals("Bob")) {
+         System.out.println("Hello " + name + ".");
 
     }
+
+}
 }
